dc0aadaf96c9b858d9be1a97a7df4ff7583f6e5c
Carlos Simon <git@csimon.dev>
2020-09-23 22:45:38 +0200

    modify packages to fit my needs

--- pkg/mdaffin/devel/files/etc/profile.d/mdaffin-devel.sh
+++
@@ -1,9 +0,0 @@
-# Node
-export PATH=$HOME/.local/share/npm/bin:$HOME/.yarn/bin:$PATH
-
-# Go
-export GOPATH=$HOME/projects/go
-export PATH=$GOPATH/bin:$PATH
-
-# Rust
-export PATH=$HOME/.cargo/bin:$PATH
